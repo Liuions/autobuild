f520cae47577679bb3b207b9383aaf735faaee99
Liuions <33196790+Liuions@users.noreply.github.com>
2023-02-13 20:35:52 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,6 +4,8 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
+svn co https://github.com/281677160/openwrt-package/trunk/luci-app-clash package/luci-app-clash
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus
 # 后台IP设置
 export Ipv4_ipaddr="192.168.168.1"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
